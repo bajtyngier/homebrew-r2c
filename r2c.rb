c7edde47e343736a0b67e0ab070728b375763f5b
Konrad Bajtyngier <konrad@vivino.com>
2024-12-20 14:32:47 +0100

    Verson 0.0.4

--- r2c.rb
+++ r2c.rb
@@ -1,8 +1,8 @@
 class R2c < Formula
   desc "Redmine to ClickUp migration tool"
   homepage "https://github.com/bajtyngier/r2c"
-  url "https://github.com/bajtyngier/r2c/archive/refs/tags/0.0.3.tar.gz"
-  sha256 "722a9d46b481328cc4f3489e73bfff9e60859c7fa1016324b1525b67fbd3730e"
+  url "https://github.com/bajtyngier/r2c/archive/refs/tags/0.0.4.tar.gz"
+  sha256 "14cf1a6dcd566215858ce6c62a2c72e6213f59de96d36cfe1da3f5a03182620b"
   depends_on "python"
 
   def install
