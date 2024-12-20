c2ff7a88489aedd4ce49027db212e865d0ff6de4
Konrad Bajtyngier <konrad@vivino.com>
2024-12-20 14:44:14 +0100

    Version 0.0.5

--- r2c.rb
+++ r2c.rb
@@ -1,13 +1,13 @@
 class R2c < Formula
   desc "Redmine to ClickUp migration tool"
   homepage "https://github.com/bajtyngier/r2c"
-  url "https://github.com/bajtyngier/r2c/archive/refs/tags/0.0.4.tar.gz"
-  sha256 "14cf1a6dcd566215858ce6c62a2c72e6213f59de96d36cfe1da3f5a03182620b"
+  url "https://github.com/bajtyngier/r2c/archive/refs/tags/0.0.5.tar.gz"
+  sha256 "97514fc526715647a03a884439f9537c586966016234e39f80c0d82843b76893"
   depends_on "python"
 
   def install
     chmod "+x", "r2c/main.py"
     bin.install "r2c/main.py" => "r2c"
-    prefix.install Dir["*"]
+    prefix.install Dir["r2c/*"]
   end
 end
