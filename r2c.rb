2f3d6c34e1d315394a35d41b2cb0ea6b00e2d6d1
Konrad Bajtyngier <konrad@vivino.com>
2025-01-02 12:17:26 +0100

    Version 1.0.0

--- r2c.rb
+++ r2c.rb
@@ -1,16 +1,16 @@
 class R2c < Formula
   desc "Redmine to ClickUp migration tool"
   homepage "https://github.com/bajtyngier/r2c"
-  url "https://github.com/bajtyngier/r2c/archive/refs/tags/0.0.6.tar.gz"
-  sha256 "6f943dc48599c9ff53cf1bcfde733e96b4d0127198b44c6a4fcd554851be3faa"
-  depends_on "python"
+  url "https://github.com/bajtyngier/r2c/archive/refs/tags/1.0.0.tar.gz"
+  sha256 "0e0f743a6b407e8914a6c3b3a01bf1edc889acc574472c18af6fad9c88824906"
+
+  depends_on "python@3"
 
   def install
-    # Ensure the r2c directory exists
-    prefix.install Dir["*"]  # Install all files in the current directory
-
-    # Install all files into r2c/ directory under Homebrew installation
-    bin.install "main.py" => "r2c"  # Symlink main.py as 'r2c'
-    prefix.install "r2c"  # Move all files into r2c/ folder if necessary
+    prefix.install Dir["*"]
+    (bin/"r2c").write <<~EOS
+      #!/bin/bash
+      exec python3 "#{prefix}/main.py" "$@"
+    EOS
   end
 end
